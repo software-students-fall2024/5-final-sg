27da1107456eb2e1ec036057f9d50558a7c345e7
Chelsea-Hodgson <csh7916@nyu.edu>
2024-12-09 12:22:46 -0500

    db cluster update

--- app.py
+++ app.py
@@ -201,6 +201,6 @@ def delete_event(event_id):
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    #app.run(debug=True, port=3000)
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=3000)
+    app.run(debug=True, port=3000)
+   # from waitress import serve
+    #serve(app, host="0.0.0.0", port=3000)
